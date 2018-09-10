ef2bfb21f61c65f6a4b09c581e6584dcd00fe7a5
Dilina Perera <dilina619@gmail.com>
2018-09-10 14:15:43 +1000

    commit

---
+++ WeatherApp/WeatherApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+	public partial class App : Application
+	{
+		
+	}
+}
